b367a0e3455bdea13f1eb9e9d34127bd31f4b856
Dave Umrysh <dave@umrysh.com>
2012-02-24 23:53:08 -0700

    Feb 24 2012

--- likeafox.py
+++ likeafox.py
@@ -1,4 +1,19 @@
-# Built for python 2.7
+#    Built for python 2.7
+
+#    Copyright 2012 Dave Umrysh
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 import MySQLdb as mdb
 import pygtk,gtk,re
 
